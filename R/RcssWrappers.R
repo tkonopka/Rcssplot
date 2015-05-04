dba32fe7799e70d885bc4e805b37cc7eb3ed75b0
Neal Fultz <njf@zestfinance.com>
2015-05-04 19:53:13 +0000

    Fixing infite recur ???

--- R/RcssWrappers.R
+++ R/RcssWrappers.R
@@ -32,7 +32,7 @@ RcssGeneric <- function(..., Rcss, Rcssclass, f) {
 
   RcssCall <- substitute(f(...))
   if(!missing(Rcss) || !(formals()$Rcss == "")){
-    css <- RcssGetProperties(Rcss, as.character(RcssCall[[1]]), Rcssclass = Rcssclass)
+    css <- RcssGetProperties(Rcss, as.character(RcssCall[[1]][[3]]), Rcssclass = Rcssclass)
     css[names(RcssCall)] <- NULL
     if(length(css) > 0) {
       i <- length(RcssCall) + seq_along(css)
@@ -40,7 +40,7 @@ RcssGeneric <- function(..., Rcss, Rcssclass, f) {
       names(RcssCall)[i] <- names(css)
     }
   }
-  eval(RcssCall, 1);
+  eval(RcssCall, -1);
 }
 
 #' RCss Wrapper functions
@@ -51,29 +51,29 @@ RcssGeneric <- function(..., Rcss, Rcssclass, f) {
 NULL
 
 towrap <- list(
-  quote(box),
-  quote(boxplot),                   
-  quote(hist),
-  quote(jpeg),
-  quote(legend),
-  quote(lines),
-  quote(mtext),
-  quote(par),
-  quote(pdf),
-  quote(plot),
-  quote(png),
-  quote(points),
-  quote(polygon),
-  quote(rect),
-  quote(stripchart),
-  quote(text),
-  quote(title)
+  quote(graphics::box),
+  quote(graphics::boxplot),                   
+  quote(graphics::hist),
+  quote(graphics::jpeg),
+  quote(graphics::legend),
+  quote(graphics::lines),
+  quote(graphics::mtext),
+  quote(graphics::par),
+  quote(graphics::pdf),
+  quote(graphics::plot),
+  quote(graphics::png),
+  quote(graphics::points),
+  quote(graphics::polygon),
+  quote(graphics::rect),
+  quote(graphics::stripchart),
+  quote(graphics::text),
+  quote(graphics::title)
 )
 
 f <- RcssGeneric
 for(symbol in towrap){
   formals(f)$f <- symbol
-  assign(paste0("Rcss", symbol), f)
+  assign(paste0("Rcss", symbol[[3]]), f)
 }
 rm(symbol, f)
 
@@ -91,7 +91,7 @@ dryer <- function(Rcss) {
   for(symbol in towrap) {
     formals(f)$f <- symbol
     formals(f)$Rcss <- CSS
-    assign(as.character(symbol), f, e)
+    assign(as.character(symbol[[3]]), f, e)
   }
   
   
